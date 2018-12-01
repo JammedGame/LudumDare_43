df2d88ed82810807c99b9e3bb9f7b674180cfc5d
Unknown <pistacj@gmail.com>
2018-12-01 19:29:55 +0100

    DJ/Gldie

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,80 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public CharacterController2D controller;
-	public float runSpeed = 60f;
-	float horizontalMove = 0f;
-	bool jump = false;
-	bool crouch = false;
+    public CharacterController2D controller;
+    public float runSpeed = 80f;
+  
+    float dirX;
 
-	void Update () {
+    [SerializeField]
+    float jumpForce = 600f, moveSpeed = 5f;
+
+    Rigidbody2D rb;
+
+    bool doubleJumpAllowed = false;
+    bool onTheGround = false;
+    bool glide = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (rb.velocity.y == 0)
+            onTheGround = true;
+        else
+            onTheGround = false;
+
+        if (onTheGround)
+            doubleJumpAllowed = true;
+
+        if (onTheGround && Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        else if (doubleJumpAllowed && Input.GetButtonDown("Jump"))
+        {
+            Jump();
+            doubleJumpAllowed = false;
+        }
+
+        dirX = Input.GetAxis("Horizontal") * moveSpeed;
+
+        if (Input.GetButtonDown("Glide"))
+        {
+            glide = true;
+        }
+        if (Input.GetButtonUp("Glide"))
+        {
+            glide = false;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        if (glide && rb.velocity.y < 0)
+            rb.gravityScale = 0.6f;
+        else
+            rb.gravityScale = 3;
+
+
+        
+            rb.velocity = new Vector2(dirX, rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0f); 
+        rb.AddForce(Vector2.up * jumpForce);
+    }
+}
+/*	void Update () {
 
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 		if (Input.GetButtonDown("Jump")) {
@@ -26,3 +93,4 @@ public class PlayerMovement : MonoBehaviour {
 		
 	}
 }
+*/
