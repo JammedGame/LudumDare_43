992ad2b601dba8a9fd6b0a725ac24daa3a4d2e78
Mladen Drobnjakovic <mladjo2505@gmail.com>
2018-12-01 21:06:22 +0100

    Fix Player double jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,44 +4,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public CharacterController2D controller;
     public float runSpeed = 80f;
   
-    float dirX;
+    public float jumpForce = 600f;
+	public float moveSpeed = 5f;
 
-    [SerializeField]
-    float jumpForce = 600f, moveSpeed = 5f;
+	public int jumpsAllowed = 2;
+
+    float dirX;
 
     Rigidbody2D rb;
 
-    bool doubleJumpAllowed = false;
-    bool onTheGround = false;
+    public int jumpCount = 0;
     bool glide = false;
     bool isJumping = false;
 
-    // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-
-        if (rb.velocity.y == 0) {
-            onTheGround = true;
-            doubleJumpAllowed = true;
-        } else {
-            onTheGround = false;
-        }
-        
-        if (doubleJumpAllowed && Input.GetButtonDown("Jump"))
+        if (jumpCount < jumpsAllowed && Input.GetButtonDown("Jump"))
         {
             isJumping = true;
-            if (!onTheGround && doubleJumpAllowed) {
-                doubleJumpAllowed = false;
-            }
+			jumpCount++;
         }
 
         dirX = Input.GetAxis("Horizontal") * moveSpeed;
@@ -54,12 +42,11 @@ public class PlayerMovement : MonoBehaviour {
         {
             glide = false;
         }
-
     }
 
     void FixedUpdate() {
         if (isJumping) {
-            Jump();
+	        Jump();
             isJumping = false;
         }
 
@@ -77,21 +64,9 @@ public class PlayerMovement : MonoBehaviour {
         rb.velocity = new Vector2(rb.velocity.x, 0f); 
         rb.AddForce(Vector2.up * jumpForce);
     }
-}
-/*	void Update () {
 
-		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-		if (Input.GetButtonDown("Jump")) {
-			jump = true;
-		}
-		
-	}
-
-	void FixedUpdate () {
-
-		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
-		jump = false;
-		
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		jumpCount = 0;
 	}
 }
-*/
