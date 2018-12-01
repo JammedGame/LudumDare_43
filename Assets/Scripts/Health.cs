6c18703e18d40426b81707c36a412a81889273e7
Grafexy <pavlovic.ivan.html@gmail.com>
2018-12-01 18:10:08 +0100

    Add Health control

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	[SerializeField] public int startingHealth = 100;
+	public int currentHealth;
+	public bool isDead;
+
+	private void Awake() {
+		isDead = false;
+		currentHealth = startingHealth;
+	}
+
+	public void TakeDamage(int amount) {
+        currentHealth -= amount;
+		if (currentHealth <= 0 && !isDead) {
+            Death();
+        }
+	}
+
+	public void healHealth(int amount) {
+        currentHealth += amount;
+		if (currentHealth > startingHealth) {
+			currentHealth = startingHealth;
+		}
+	}
+
+	void Death() {
+		isDead = true;
+	}
+}
