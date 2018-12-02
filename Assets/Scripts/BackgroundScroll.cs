f6713eceac924fb4c3f8eab0b4ef8d227499df1a
Mladen Drobnjakovic <mladjo2505@gmail.com>
2018-12-02 14:56:27 +0100

    Background  scroll

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour {
+
+	public GameObject follow;
+
+	private float y;
+
+	private RectTransform rectTransform;
+	void Start() {
+		rectTransform = GetComponent<RectTransform>();
+		y = follow.transform.position.y - rectTransform.position.y;
+	}
+
+	void Update () {
+		Vector3 _new = rectTransform.position;
+		_new.y = (follow.transform.position.y - y) * 0.5f;
+		rectTransform.position = _new;
+	}
+}
