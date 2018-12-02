d47b2df2185b08db2be174dba08445450a5e86d8
Unknown <pistacj@gmail.com>
2018-12-02 18:51:09 +0100

    Fontana

---
+++ Assets/Scripts/Fontana.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fontana : MonoBehaviour {
+
+  
+    public GameObject player;
+    private float hps=25f;
+
+	
+	void FixedUpdate () {
+
+        if (player)
+            player.GetComponent<Health>().healHealth(hps * Time.deltaTime);
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player") 
+        {
+           
+            player = collider.gameObject;
+         
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            player = null;
+         
+        }
+    }
+
+}
