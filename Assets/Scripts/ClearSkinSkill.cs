f90bd835b11531d682da7470beccfda62cbc00c2
Unknown <pistacj@gmail.com>
2018-12-03 23:57:42 +0100

    SkillsEnabled

---
+++ Assets/Scripts/ClearSkinSkill.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClearSkinSkill : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject skill;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            player = collider.gameObject;
+            player.GetComponent<PlayerMovement>().canClearScreen = true;
+
+
+            var image = skill.GetComponent<Image>();
+            var tempColor = image.color;
+            tempColor.a = 1f;
+            image.color = tempColor;
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/DoubleJumpSkill.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoubleJumpSkill : MonoBehaviour {
+
+        public GameObject player;
+        public GameObject skill;
+
+    void OnTriggerEnter2D(Collider2D collider)
+        {
+            if (collider.gameObject.tag == "Player")
+            {
+                 player = collider.gameObject;
+                 player.GetComponent<PlayerMovement>().jumpsAllowed =2;
+
+           
+                 var image = skill.GetComponent<Image>();
+                 var tempColor = image.color;
+                 tempColor.a = 1f;
+                 image.color = tempColor;
+
+                 Destroy(gameObject);
+            }
+        }
+}
+
+    
+
+
+
+
