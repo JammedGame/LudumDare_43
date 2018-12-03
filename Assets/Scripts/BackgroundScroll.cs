066f63b624624fc54ca39922e03d17e8bf3d2819
Grafexy <pavlovic.ivan.html@gmail.com>
2018-12-03 22:36:55 +0100

    BiomChanger

--- Assets/Scripts/BackgroundScroll.cs
+++ Assets/Scripts/BackgroundScroll.cs
@@ -1,22 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BackgroundScroll : MonoBehaviour {
 
+	public int activeBiomIndex = 0;
+	public int currentBiomIndex = 0;
+
 	public GameObject follow;
 
-	private float y;
+	private float[] y;
 
-	private RectTransform rectTransform;
+	private RectTransform[] rectTransforms;
+
+	public float fadeTime = 1f;
+	private float timeToFade;
+	private bool startTranstion = false;
 	void Start() {
-		rectTransform = GetComponent<RectTransform>();
-		y = follow.transform.position.y - rectTransform.position.y;
+		rectTransforms = GetComponentsInChildren<RectTransform>(true);
+		y = new float[rectTransforms.Length];
+
+		for (int i = 1; i < rectTransforms.Length; i++) {
+			y[i] = follow.transform.position.y - rectTransforms[i].position.y;
+		}		
 	}
 
 	void Update () {
-		Vector3 _new = rectTransform.position;
-		_new.y = (follow.transform.position.y - y) * 0.5f;
-		rectTransform.position = _new;
+		for (int i = 1; i < rectTransforms.Length; i++) {
+			Vector3 _new = rectTransforms[i].position;
+			_new.y = (follow.transform.position.y - y[i]) * 0.5f;
+			rectTransforms[i].position = _new;
+		}
+
+	}
+
+	private void FixedUpdate() {
+		if (startTranstion) {
+			timeToFade -= Time.deltaTime;
+			Image[] images = GetComponentsInChildren<Image>();
+			Image biom = GetComponentsInChildren<Image>()[currentBiomIndex];
+			Debug.Log(biom.color);
+			Color temp_biom_color = biom.color;
+			temp_biom_color.a = timeToFade;
+			biom.color = temp_biom_color;
+			
+			Image biom2 = GetComponentsInChildren<Image>()[activeBiomIndex];
+			Color temp_biom_color2 = biom2.color;
+			temp_biom_color2.a = 1f - timeToFade;
+			biom2.color = temp_biom_color2;
+
+			if (timeToFade < 0f) {
+				startTranstion = false;
+			}
+		}
+	}
+
+	public void changeBiom(int biomIndex, int prevBiomIndex) {
+		currentBiomIndex = activeBiomIndex;
+		if (biomIndex == activeBiomIndex) {
+			activeBiomIndex = prevBiomIndex;
+		} else {
+			activeBiomIndex = biomIndex;
+		}
+
+		timeToFade = fadeTime;
+		startTranstion = true;
 	}
 }
---
+++ Assets/Scripts/Biomchanger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Biomchanger : MonoBehaviour {
+
+	public int prevBiom = 0;
+	public int biom = 1;
+	public GameObject background;
+
+	private void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject.tag == "Player" && collider.isTrigger == false) {
+			background.GetComponent<BackgroundScroll>().changeBiom(biom, prevBiom);
+		}
+	}
+}
